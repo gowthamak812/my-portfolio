8a7446ced4a8477a9d253690aea30ca08cc9d223
gowtham <gowthamak812@gmail.com>
2025-05-25 21:32:00 +0530

    App title changed

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Banner from './components/banner';
 import About from './components/about';
 import Skills from './components/skills';
 import Services from './components/services';
-import Freelancer from './components/footer';
+import Footer from './components/footer';
 import Contact from './components/contact-us';
 
 
